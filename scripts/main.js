b0df4367dff2e1363ee44fb3cb579f6fe9b0ea52
RKovl <ryan.kovlaske@gmail.com>
2023-01-25 15:44:26 -0800

    data added

---
+++ scripts/main.js
@@ -0,0 +1,59 @@
+let data = {
+	R6S: [
+		{
+			name: "Bank",
+			dataname: "Bank",
+			desc: "",
+			img: "../images/R6S_Bank.webp"
+		},
+		{
+			name: "Border",
+			dataname: "Border",
+			desc: "",
+			img: "../images/R6S_Border.webp"
+		},
+		{
+			name: "Chalet",
+			dataname: "Chalet",
+			desc: "",
+			img: "../images/R6S_Chalet.webp"
+		},
+		{
+			name: "Club House",
+			dataname: "Club_House",
+			desc: "",
+			img: "../images/R6S_Club_House.webp"
+		},
+		{
+			name: "Kafe",
+			dataname: "Kafe",
+			desc: "",
+			img: "../images/R6S_Kafe.webp"
+		},
+		{
+			name: "Oregon",
+			dataname: "Oregon",
+			desc: "",
+			img: "../images/R6S_Oregon.webp"
+		},
+		{
+			name: "Skyscraper",
+			dataname: "Skyscraper",
+			desc: "",
+			img: "../images/R6S_Skyscraper.webp"
+		},
+		{
+			name: "Theme Park",
+			dataname: "Theme_Park",
+			desc: "",
+			img: "../images/R6S_Theme_Park.webp"
+		},
+		{
+			name: "Villa",
+			dataname: "Villa",
+			desc: "",
+			img: "../images/R6S_Villa.webp"
+		}
+	],
+	VAL: []
+};
