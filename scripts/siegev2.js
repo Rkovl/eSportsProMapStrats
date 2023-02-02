32ce4d2b8796e813f70d20bebbf406560ac9555f
RKovl <ryan.kovlaske@gmail.com>
2023-02-01 21:59:02 -0800

    nav button functionality implemented

--- scripts/siegev2.js
+++ scripts/siegev2.js
@@ -2,27 +2,27 @@ let data = {
     bank:[
         {
             name: 'Default',
-            id: 'mapA',
+            id: '0',
             image: ['../../images/siege/bank/R6S_Bank_Default_1.jpg','../../images/siege/bank/R6S_Bank_Default_2.jpg','../../images/siege/bank/R6S_Bank_Default_3.jpg']
         },
         {
             name: 'B Lockers/B CCTV Room',
-            id: 'mapB',
+            id: '1',
             image: []
         },
         {
             name: '1F Teller\'s Office/1F Archives',
-            id: 'mapC',
+            id: '2',
             image: []
         },
         {
             name: '1F Staff Room/1F Open Area',
-            id: 'mapD',
+            id: '3',
             image: []
         },
         {
             name: '2F Executive Lounge/2F CEO Office',
-            id: 'mapE',
+            id: '4',
             image: []
         }
     ],
@@ -219,16 +219,30 @@ console.log(URI, "URI")
 let imageContainer = document.querySelector('#imageContainer')
 let navContainer = document.querySelector('#navContainer')
 let mapSub
+let uriID
+
+function displayMap(id) {
+    imageContainer.innerHTML = ``
+    console.log("inside onclick", uriID, Number(id))
+    for(const image of data[uriID][id].image){
+        imageContainer.innerHTML +=`
+        <div class="card bg-dark text-white col p-1 border">
+            <img class="img-fluid" src="${image}" alt="">
+        </div>
+        `
+    }
+    
+}
 
 if(URI.includes("bank")){
     console.log("inside bank if")
-
+    uriID = "bank"
     for(const map of data.bank){
         console.log(map)
 
         if(map.name == 'Default'){
             navContainer.innerHTML =`
-            <button class="nav-link active" id="${map.id}" data-bs-toggle="tab"  type="button" role="tab"  aria-selected="true">${map.name}</button>
+            <button class="nav-link active" id="${map.id}" data-bs-toggle="tab"  type="button" role="tab"  aria-selected="true" onclick="displayMap(this.id)">${map.name}</button>
             `
             for(const image of map.image){
                 imageContainer.innerHTML +=`
@@ -240,7 +254,7 @@ if(URI.includes("bank")){
         }
         else{
             navContainer.innerHTML +=`
-            <button class="nav-link" id="${map.id}" data-bs-toggle="tab"  type="button" role="tab"  aria-selected="false">${map.name}</button>
+            <button class="nav-link" id="${map.id}" data-bs-toggle="tab"  type="button" role="tab"  aria-selected="false" onclick="displayMap(this.id)">${map.name}</button>
             `
         }
 
